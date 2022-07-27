466367944469e631b9a1ec58da9cc7e1407ea835
JeongHyunHeo <jeonghyunheo619@gmail.com>
2022-07-27 14:59:11 +0900

    new component added: (initial)navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,19 +5,26 @@ import styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
         return (
-                <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-primary justify-content-center">
-                        <a className="navbar-brand" href="" id="sitename">
-                                <Logo className="logo"/>
-                        </a>
+
+                <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark justify-content-center">
+                        <div className="container mx-auto">
+                                <div className="grid grid-cols-3 flex items-center justify-between">
+                                        <div className="justify-left">
+                                                <Image src='/logo.png' alt='logo' height={150} width={150} />
+                                        </div>
+                                        <a className="navbar-brand justify-center" href="" id="sitename">
+                                                <Logo />
+                                        </a>
+                                </div>
+                        </div>
+
                 </nav>
         )
 }
 
 const Logo = () => {
         return (
-                <h2>
-                        Timatri
-                </h2> 
+                <Image src='/logo_name.png' alt='logo_name' height={100} width={450}/>
         )
 }
 export default Navbar;
